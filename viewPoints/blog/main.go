2e571ee5c4485664d0d8162a4075f5dff6c80790
Volodymyr Melnyk <vmelnyk@tradetracker.com>
2018-11-26 16:00:57 +0200

    updates dependencies

--- viewPoints/blog/main.go
+++ viewPoints/blog/main.go
@@ -3,32 +3,18 @@ package main
 import (
 	"net/http"
 
-	"github.com/egoholic/tribune/mongoSession"
 	"github.com/gorilla/mux"
-	"github.com/labstack/echo"
 	mgo "gopkg.in/mgo.v2"
 )
-
-type ExtendedContext struct {
-	echo.Context
-	MongoDBSession *mgo.Session
-}
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", dumbHandler)
-	http.Handle("/", mongoSession.WithMongoSession(router))
+	http.Handle("/", router)
 }
 
 func dumbHandler(w http.ResponseWriter, r *http.Request) {
 
-}
-
-func extendContextMiddleware(h echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		newContext := &ExtendedContext{c, initMongoDBSession()}
-		return h(newContext)
-	}
 }
 
 func initMongoDBSession() *mgo.Session {
