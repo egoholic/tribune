817e15539001e775b9a6387ff66871f47cd5b275
Volodymyr Melnyk <vmelnyk@tradetracker.com>
2018-11-29 17:36:25 +0200

    arch change

--- targets/blog/main.go
+++ targets/blog/main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"net/http"
 
-	rr "github.com/egoholic/tribune/space/domains/reading/routing"
+	publishing "github.com/egoholic/tribune/outerspace_adapters/http/domains/publishing/handlers"
+	reading "github.com/egoholic/tribune/outerspace_adapters/http/domains/reading/handlers"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	readingRouter := rr.Build()
-	http.Handle("/", readingRouter)
+	rtr := mux.NewRouter()
+
+	rtr.HandleFunc("/publications", publishing.CreatePublication).Methods("POST")
+	rtr.HandleFunc("/publications/{slug:[0-9a-z-]}", publishing.UpdatePublication).Methods("PATCH")
+	rtr.HandleFunc("/publications", reading.RenderAllWithPagination).Methods("GET")
+	rtr.HandleFunc("/publications/{slug:[0-9a-z-]}", reading.RenderOneBySlug).Methods("GET")
+	rtr.HandleFunc("/", reading.RenderTheLatestPublished).Methods("GET")
+
+	http.Handle("/", rtr)
 }
